ebeb83c89f820488a5d3aa29f058ec82025f7282
mani1232 <support@worldmandia.cc>
2025-03-11 23:56:26 +0100

    Fix

--- kpi-api/src/main/kotlin/cc/worldmandia/api/inventory/impl/feature/PagedInventory.kt
+++
@@ -1,6 +0,0 @@
-package cc.worldmandia.api.inventory.impl.feature
-
-import cc.worldmandia.api.inventory.impl.ImplInventory
-import net.kyori.adventure.text.Component
-
-class PagedInventory(title: Component) : ImplInventory(title)
--- kpi-api/src/main/kotlin/cc/worldmandia/api/inventory/impl/feature/StorageInventory.kt
+++ kpi-api/src/main/kotlin/cc/worldmandia/api/inventory/impl/feature/StorageInventory.kt
@@ -1,6 +1,7 @@
 package cc.worldmandia.api.inventory.impl.feature
 
 import cc.worldmandia.api.inventory.impl.ImplInventory
+import cc.worldmandia.api.inventory.type.BaseType
 import net.kyori.adventure.text.Component
 
-class StorageInventory(title: Component) : ImplInventory(title)
+class StorageInventory(title: Component, type: BaseType) : ImplInventory(title, type)
